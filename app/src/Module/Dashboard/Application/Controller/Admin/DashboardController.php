7fc3cdd5ddc9dd41e3a0d493b9ccdd0a00901d82
Rafał Mikołajun <root@rmweb.pl>
2023-12-17 22:13:43 +0100

    add DashboardController

---
+++ app/src/Module/Dashboard/Application/Controller/Admin/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Module\Dashboard\Application\Controller\Admin;
+
+use App\Module\Admin\Application\Controller\AbstractAdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class DashboardController extends AbstractAdminController
+{
+    public function getDashboard(Request $request): Response
+    {
+        return $this->render('admin/dashboard/dashboard.html.twig');
+    }
+}
