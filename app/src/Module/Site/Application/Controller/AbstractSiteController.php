8848d1901aabc222b7399963de94f38f55d3e91a
Rafał Mikołajun <root@rmweb.pl>
2023-12-18 17:31:29 +0100

    add AbstractSiteController

---
+++ app/src/Module/Site/Application/Controller/AbstractSiteController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Module\Site\Application\Controller;
+
+use App\Module\Core\Application\Controller\AbstractAppController;
+
+abstract class AbstractSiteController extends AbstractAppController
+{
+}
