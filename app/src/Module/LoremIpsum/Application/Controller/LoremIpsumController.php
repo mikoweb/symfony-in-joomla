00c6076f3d7c3942e5173cc57b2cb441b9d14766
Rafał Mikołajun <root@rmweb.pl>
2023-12-18 17:50:12 +0100

    add site LoremIpsumController

---
+++ app/src/Module/LoremIpsum/Application/Controller/LoremIpsumController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Module\LoremIpsum\Application\Controller;
+
+use App\Module\Site\Application\Controller\AbstractSiteController;
+use Joomla\CMS\Document\Document;
+use Joomla\CMS\Factory;
+use Symfony\Component\HttpFoundation\Response;
+
+final class LoremIpsumController extends AbstractSiteController
+{
+    public function index(): Response
+    {
+        /** @var Document $document */
+        $document = Factory::getApplication()->getDocument();
+        $document->setTitle($document->getTitle() . ' - Lorem Ipsum');
+
+        return $this->render('site/lorem_ipsum.html.twig');
+    }
+}
